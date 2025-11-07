e60c5a66bc1ceadc47583871aedc1e4f4b44c51c
Kayky Gibran <72230429+KGMats@users.noreply.github.com>
2025-11-07 11:50:21 -0300

    fixed branch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -21,6 +21,7 @@ const config: Config = {
   organizationName: 'KGMats',
   trailingSlash: false,
   onBrokenLinks: 'throw',
+  deploymentBranch: 'master',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
